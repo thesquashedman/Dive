3a6a1fb822bd9e19bf6124269eb93b6444c570c8
davionli <davionli1007@gmail.com>
2023-07-02 17:29:05 -0700

    mobile movement

--- Assets/Scripts/MobileController.cs
+++ Assets/Scripts/MobileController.cs
@@ -7,27 +7,46 @@ public class MobileController : MonoBehaviour
 {
     public Weapon curentWeapon;
     public Button attack;
+    public float curTime;
+    public float maxTime;
+    public bool timerOn = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        maxTime = 0.5f;
+        Button btn = attack.GetComponent<Button>();
+		btn.onClick.AddListener(Attack);
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        // Debug.Log(timerOn);
+        if (timerOn) {
+            curTime -= Time.deltaTime;
+            // Debug.Log(curTime);
+            if (curTime <= 0.0f)
+                TimerEnded();
+        }
     }
 
-    void FixedUpdate() {
-        Button btn = attack.GetComponent<Button>();
-		btn.onClick.AddListener(Attack);
-    }
+    
 
     void Attack() {
-        Debug.Log("Attack!!!!!");
-        curentWeapon.Attack();
-        Debug.Log("Stop Attack!!!!!");
-        curentWeapon.StopAttack();
+        if (!timerOn) {
+            Debug.Log("Attack!!!!!");
+            curentWeapon.Attack();
+            curTime = maxTime;
+            timerOn = true;
+        }
     }
+
+    void TimerEnded() {
+        // if (timerOn) {
+            Debug.Log("Stop Attack!!!!!");
+            curentWeapon.StopAttack();
+            timerOn = false;
+        // }
+    }
+    
 }
