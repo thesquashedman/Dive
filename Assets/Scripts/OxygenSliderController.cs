aeff92bc857a2799f87021a95dce74b54c84fc25
Mikhail10920 <ermolenko.10920@gmail.com>
2023-07-16 03:14:41 +0500

    Oxygen scale

---
+++ Assets/Scripts/OxygenSliderController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class OxygenSliderController : MonoBehaviour
+{
+    public OxygenSystem oxSys;
+    float maxOxyScale = 10;
+    public float oxLevel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        maxOxyScale = this.gameObject.transform.localScale.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //this.gameObject.GetComponent<Transform>().transform.localScale.y = oxSys.GetOxygenLevel();
+
+        oxLevel = (oxSys.GetOxygenLevel() / 100) * maxOxyScale;
+
+        this.gameObject.transform.localScale = new Vector3(this.gameObject.transform.localScale.x, oxLevel, this.gameObject.transform.localScale.z);
+    }
+
+
+}
