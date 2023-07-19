3c1d0c9b025e5289b985d8a4cc6ffd940ce808de
Pavel Peev <76268971+thesquashedman@users.noreply.github.com>
2023-07-19 15:50:35 -0700

    Bug fix for Global Lighting handler

--- Assets/Scripts/LightSystemTooling/GlobalLightHandler.cs
+++ Assets/Scripts/LightSystemTooling/GlobalLightHandler.cs
@@ -5,11 +5,23 @@ using UnityEngine.Rendering.Universal;
 
 public class GlobalLightHandler : MonoBehaviour
 {
+    struct SpriteAndColor
+    {
+        public SpriteRenderer renderer;
+        public Color startingColor;
+    }
     public static GlobalLightHandler current;
     public RenderTexture globalLighting;
     public Light2D globalLight; 
+    List<SpriteAndColor> spriteRenderers = new List<SpriteAndColor>();
+
+    public float lightMax = 1;
+    public float lightMin = 0;
+
+    public float colorDarkenMaxPercentage = 0.5f;
 
     public float lerpspeed = 0.05f;
+    public float colorLerpSpeed = 0.25f;
     private void Awake() {
         //Singleton pattern
         if(current != null && current != this) {
@@ -17,6 +29,19 @@ public class GlobalLightHandler : MonoBehaviour
         } else {
             current = this;
         }
+    }
+    private void Start() {
+        GameObject[] backgrounds = GameObject.FindGameObjectsWithTag("Background");
+        foreach(GameObject background in backgrounds)
+        {
+            SpriteAndColor temp = new SpriteAndColor();
+            
+            temp.renderer = background.GetComponent<SpriteRenderer>();
+            temp.startingColor = background.GetComponent<SpriteRenderer>().color;
+            spriteRenderers.Add(temp);
+        }
+        //spriteRenderers = GetComponents<SpriteRenderer>();
+
     }
     
 
@@ -26,7 +51,24 @@ public class GlobalLightHandler : MonoBehaviour
         Texture2D temp = GetRTPixels(globalLighting);
         //float previousIntensity = globalLight.intensity;
         float intensity = temp.GetPixel(globalLighting.width/2, globalLighting.height/2).grayscale;
-        globalLight.intensity = Mathf.Lerp(globalLight.intensity, intensity, lerpspeed);
+        float lerp = Mathf.Lerp((globalLight.intensity - lightMin) / (lightMax -lightMin), intensity, lerpspeed);
+        globalLight.intensity = lerp * (lightMax - lightMin) + lightMin;
+        
+        foreach (SpriteAndColor sprite in spriteRenderers)
+        {
+            //docs.unity3d.com/ScriptReference/Color.RGBToHSV.html
+            //docs.unity3d.com/ScriptReference/Color.HSVToRGB.html
+            float H, S, V;
+            Color.RGBToHSV(sprite.startingColor.linear, out H, out S, out V);
+            V *= 1 - (colorDarkenMaxPercentage * 1 - intensity);
+            Debug.Log(V);
+            sprite.renderer.color = Color.Lerp(sprite.renderer.color, Color.HSVToRGB(H, S, V).gamma, colorLerpSpeed);
+            /*
+            Color newColor = (sprite.startingColor * (1 - darkenPercent * (1 - lerp))).gamma;
+            newColor.a = sprite.startingColor.a;
+            sprite.renderer.color = newColor;
+            */
+        }
         Destroy(temp);
     }
     static public Texture2D GetRTPixels(RenderTexture rt)
