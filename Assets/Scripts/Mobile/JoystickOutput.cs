c452ab1e955083a6c139c17aa5cada9785ab4417
davionli <davionli1007@gmail.com>
2023-07-25 07:00:34 -0700

    Menu Prefab

--- Assets/Scripts/Mobile/JoystickOutput.cs
+++ Assets/Scripts/Mobile/JoystickOutput.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class JoystickOutput : MonoBehaviour
 {
-    // public static JoystickOutput current;
+    public static JoystickOutput current;
     public Joystick joystick_l;
     public Joystick joystick_r;
     public Vector3 direction_l;
@@ -15,16 +15,16 @@ public class JoystickOutput : MonoBehaviour
     // bool switchWeaponIsClicked;
 
 
-    //  private void Awake() {
-    //     Debug.Log("EventManager Active.");
-    //     //Singleton pattern
-    //     if(current != null && current != this) {
-    //         Destroy(this);
-    //     } else {
-    //         current = this;
-    //         //Debug.Log("EventManager Active.");
-    //     }
-    // }
+     private void Awake() {
+        Debug.Log("EventManager Active.");
+        //Singleton pattern
+        if(current != null && current != this) {
+            Destroy(this);
+        } else {
+            current = this;
+            //Debug.Log("EventManager Active.");
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -35,7 +35,11 @@ public class JoystickOutput : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        direction_l = Vector3.up * joystick_l.Vertical + Vector3.right * joystick_l.Horizontal;
+        if (Mathf.Abs(joystick_l.Vertical) >= 0.05 && Mathf.Abs(joystick_l.Horizontal) >= 0.05)
+            direction_l = Vector3.up * joystick_l.Vertical + Vector3.right * joystick_l.Horizontal;
+        else
+            direction_l = Vector3.zero;
+
         direction_r = Vector3.up * joystick_r.Vertical + Vector3.right * joystick_r.Horizontal;
 
     }
