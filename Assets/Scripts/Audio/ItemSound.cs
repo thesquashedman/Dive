42dfc084d32c342637d38a8f7ee0c86349fc0d01
Laetheron <92497112+Laetheron@users.noreply.github.com>
2023-08-03 17:55:22 -0700

    AudioTweaks

--- Assets/Scripts/Audio/ItemSound.cs
+++ Assets/Scripts/Audio/ItemSound.cs
@@ -7,18 +7,11 @@ Written by Justin.
 
 Plays a sound before the item is picked up.
 */
-public class ItemSound : EntitySound
+public class ItemSound : MonoBehaviour
 {
-    // Start is called before the first frame update
-    public override void Start()
-    {
-        audioNames.Add("ItemPickup_2");
-        base.Start();
-    }
-
     //Called onDestroy
     //Should be when the item is picked up
-    void OnDestroy() {
-        audios[0].Play();
+    void OnDisable() {
+        AudioManager.instance.Play("ItemPickup_2");
     }
 }
