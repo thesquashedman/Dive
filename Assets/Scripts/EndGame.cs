40452c14a3af9c79e4fba1d9170588d05ed65221
Pavel Peev <76268971+thesquashedman@users.noreply.github.com>
2023-08-10 19:12:23 -0700

    LevelOne now connects to level 2

---
+++ Assets/Scripts/EndGame.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject winMessage;
+    
+    bool isDone = false;
+    void Start()
+    {
+        EventManager.current.onLevelWin += Win;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void Win()
+    {
+        isDone = true;
+    }
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.CompareTag("Player"))
+        {
+            if(isDone)
+            {
+                winMessage.SetActive(true);
+                PavelPlayerSettingStates.current.GetComponent<PavelPlayerHealth>().enabled = false;
+            }
+        }
+    }
+}
